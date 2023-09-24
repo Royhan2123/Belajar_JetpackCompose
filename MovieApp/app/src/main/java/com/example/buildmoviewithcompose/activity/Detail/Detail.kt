a719d0191d208a6abfd327fd7691af80a25f8ed3
Royhan <royhankeren12@gmail.com>
2023-09-25 00:26:42 +0700

    HorizontalScrolableImageView

--- MovieApp/app/src/main/java/com/example/buildmoviewithcompose/activity/Detail/Detail.kt
+++ MovieApp/app/src/main/java/com/example/buildmoviewithcompose/activity/Detail/Detail.kt
@@ -1,16 +1,26 @@
 package com.example.buildmoviewithcompose.activity.Detail
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
@@ -22,15 +32,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import coil.compose.rememberImagePainter
+import com.bawp.movieapp.model.Movie
 import com.bawp.movieapp.model.getMovies
+import com.example.buildmoviewithcompose.widgets.CardMovie
 
-@Preview
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -56,7 +68,8 @@ fun DetailScreen(navController: NavController, movieId: String?) {
                         Text(text = "Detail", style = TextStyle(
                             fontSize = 20.sp,
                             color = Color.Black
-                        ))
+                        )
+                        )
                     }
                 }, colors = TopAppBarDefaults.largeTopAppBarColors(
                     Color.White
@@ -67,16 +80,44 @@ fun DetailScreen(navController: NavController, movieId: String?) {
             modifier = Modifier
                 .fillMaxSize()
                 .padding(top = 50.dp),
+            color = Color.White
         ) {
             Column (modifier = Modifier
                 .fillMaxSize()
                 .padding(20.dp),
                 horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center) {
-                Text(text = newMovieList[0].title, style = TextStyle(
-                    fontSize = 20.sp,
-                    fontFamily = FontFamily.Monospace,
-                ))
+                verticalArrangement = Arrangement.Top) {
+                CardMovie(movie = newMovieList.first())
+                Spacer(modifier = Modifier.height(10.dp))
+                Divider()
+                HorizontalScrolableImageView(newMovieList)
+            }
+        }
+    }
+}
+
+@Composable
+private fun HorizontalScrolableImageView(newMovieList: List<Movie>) {
+    LazyRow {
+        items(newMovieList[0].images) { image ->
+            Card(
+                modifier = Modifier
+                    .padding(12.dp)
+                    .size(200.dp),
+                elevation = CardDefaults
+                    .cardElevation(10.dp),
+                colors = CardDefaults
+                    .cardColors(Color.White),
+                shape = RoundedCornerShape(
+                    corner = CornerSize(12.dp)
+                )
+            ) {
+                Image(
+                    modifier = Modifier.size(200.dp),
+                    contentScale = ContentScale.Crop,
+                    painter = rememberImagePainter(data = image),
+                    contentDescription = "Movie Poster"
+                )
             }
         }
     }
