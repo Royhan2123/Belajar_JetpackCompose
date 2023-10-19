597c51b375483eb56517ca706a3573b919fb9cad
Royhan <royhankeren12@gmail.com>
2023-10-19 19:06:34 +0700

    splash-screen

---
+++ LatihanViewModel/app/src/main/java/com/example/latihanviewmodel/SplashScreen.kt
@@ -0,0 +1,15 @@
+package com.example.latihanviewmodel
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun SplashScreen(){
+
+}
+
+@Preview
+@Composable
+fun PreviewSplashScreen(){
+
+}
