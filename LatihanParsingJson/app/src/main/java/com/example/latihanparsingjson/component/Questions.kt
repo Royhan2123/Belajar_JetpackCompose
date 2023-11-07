5dea6367aac87771bb424544f53b9cb92413d9e8
Royhan <royhankeren12@gmail.com>
2023-11-07 10:19:45 +0700

    Radio button

--- LatihanParsingJson/app/src/main/java/com/example/latihanparsingjson/component/Questions.kt
+++ LatihanParsingJson/app/src/main/java/com/example/latihanparsingjson/component/Questions.kt
@@ -2,19 +2,33 @@ package com.example.latihanparsingjson.component
 
 import android.util.Log
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.RadioButton
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.PathEffect
 import androidx.compose.ui.text.ParagraphStyle
 import androidx.compose.ui.text.SpanStyle
@@ -25,6 +39,8 @@ import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.latihanparsingjson.model.Question
+import com.example.latihanparsingjson.model.QuestionItem
 import com.example.latihanparsingjson.screens.QuestionViewModel
 import com.example.latihanparsingjson.util.AppColors
 
@@ -43,16 +59,29 @@ fun Questions(viewModel: QuestionViewModel) {
     Log.d("SIZE", "Question : ${questions?.size}")
 }
 
-@Preview
+//@Preview
 @Composable
-fun QuestionDisplay() {
-val pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f,10f),
-    0f)
+fun QuestionDisplay(
+    question: QuestionItem,
+    questionIndex: MutableState<Int>,
+    viewModel: QuestionViewModel,
+    onNextClicked: (Int) -> Unit,
+) {
+    val answerState = remember(question) {
+        mutableStateOf<Int?>(null)
+    }
+
+    val choicesState = remember(question) {
+        question.choices.toMutableList()
+    }
+
+    val pathEffect = PathEffect.dashPathEffect(
+        floatArrayOf(10f, 10f), 0f
+    )
     Surface(
         modifier = Modifier
             .fillMaxSize()
-            .padding(4.dp),
-        color = AppColors.mDarkPurple
+            .padding(4.dp), color = AppColors.mDarkPurple
     ) {
         Column(
             modifier = Modifier.padding(12.dp),
@@ -60,7 +89,52 @@ val pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f,10f),
             horizontalAlignment = Alignment.Start
         ) {
             QuestionTracker()
-            DrawDottedLine(pathEffect = pathEffect )
+            DrawDottedLine(pathEffect)
+            Column {
+                Text(
+                    modifier = Modifier
+                        .align(alignment = Alignment.Start)
+                        .fillMaxHeight(0.3f),
+                    text = "What's the meaning of all this?",
+                    fontSize = 17.sp,
+                    fontWeight = FontWeight.Bold,
+                    lineHeight = 22.sp,
+                    color = AppColors.mWhite
+                )
+                choicesState.forEachIndexed { index, answerText ->
+                    Row(
+                        modifier = Modifier
+                            .padding(3.dp)
+                            .fillMaxWidth()
+                            .height(45.dp)
+                            .border(
+                                width = 4.dp,
+                                brush = Brush.linearGradient(
+                                    colors = listOf(
+                                        AppColors.mDarkPurple, AppColors.mOffDarkPurple
+                                    )
+                                ),
+                                shape = RoundedCornerShape(15.dp),
+                            )
+                            .clip(
+                                RoundedCornerShape(
+                                    topStartPercent = 50,
+                                    topEndPercent = 50,
+                                    bottomEndPercent = 50,
+                                    bottomStartPercent = 50
+                                )
+                            )
+                            .background(Color.Transparent),
+                        verticalAlignment = Alignment.CenterVertically,
+                    ) {
+                        RadioButton(
+                            selected = (answerState.value == index),
+                            onClick = {
+
+                            })
+                    }
+                }
+            }
         }
     }
 }
@@ -76,15 +150,14 @@ fun DrawDottedLine(pathEffect: PathEffect) {
             color = AppColors.mLightGray,
             start = Offset(0f, 0f),
             end = Offset(size.width, y = 0f),
-            pathEffect = pathEffect)
+            pathEffect = pathEffect
+        )
     }
 }
 
-
 @Composable
 fun QuestionTracker(
-    conter: Int = 10,
-    outOff: Int = 100
+    conter: Int = 10, outOff: Int = 100
 ) {
     Text(text = buildAnnotatedString {
         withStyle(
@@ -94,9 +167,7 @@ fun QuestionTracker(
         ) {
             withStyle(
                 style = SpanStyle(
-                    color = AppColors.mLightGray,
-                    fontWeight = FontWeight.Bold,
-                    fontSize = 27.sp
+                    color = AppColors.mLightGray, fontWeight = FontWeight.Bold, fontSize = 27.sp
                 )
             ) {
                 append("Question $conter/")
