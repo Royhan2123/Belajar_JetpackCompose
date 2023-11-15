e5ded0b5276e29198e918464b5b8da041c915b3c
Royhan <royhankeren12@gmail.com>
2023-11-15 14:13:44 +0700

    splash Screen

--- MyApplication/app/build.gradle.kts
+++ MyApplication/app/build.gradle.kts
@@ -1,17 +1,14 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    kotlin("kapt")
-    //id("dagger.hilt.android.plugin")
 }
 
 android {
-    namespace = "com.example.weatherapi"
+    namespace = "com.example.myapplication"
     compileSdk = 33
 
-
     defaultConfig {
-        applicationId = "com.example.weatherapi"
+        applicationId = "com.example.myapplication"
         minSdk = 28
         targetSdk = 33
         versionCode = 1
@@ -53,30 +50,8 @@ android {
 }
 
 dependencies {
-    val roomVersion = "2.6.0"
 
-    // HILT
-    implementation("com.google.dagger:hilt-android:2.48.1")
-    //kapt ("com.google.dagger:hilt-android-compiler:2.48")
-    implementation("androidx.hilt:hilt-navigation-fragment:1.1.0")
-    //kapt("androidx.hilt:hilt-compiler:1.0.0")
-
-    // ROOM
-
-    //noinspection KaptUsageInsteadOfKsp
-    //kapt("androidx.room:room-compiler:$roomVersion")
-    implementation("androidx.room:room-runtime:$roomVersion")
-    annotationProcessor("androidx.room:room-compiler:$roomVersion")
-    implementation("androidx.room:room-ktx:$roomVersion")
-
-    // Coroutines
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.7.3")
-
-
-
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.0")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
--- WeatherApi/app/src/main/java/com/example/weatherapi/data/AppModule.kt
+++
@@ -1,5 +0,0 @@
-package com.example.weatherapi.data
-
-object AppModule {
-
-}
