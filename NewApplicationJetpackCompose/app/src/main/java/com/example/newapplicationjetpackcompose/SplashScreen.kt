3da9d41fc9ca3f82ccd88592b1be142fd28a66f1
Royhan <royhankeren12@gmail.com>
2024-02-22 20:25:33 +0700

    splash screen

--- NewApplicationJetpackCompose/app/src/main/java/com/example/newapplicationjetpackcompose/SplashScreen.kt
+++ NewApplicationJetpackCompose/app/src/main/java/com/example/newapplicationjetpackcompose/SplashScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -12,9 +13,22 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.example.newapplicationjetpackcompose.navigation.NavigationScreen
+import kotlinx.coroutines.delay
 
 @Composable
 fun SplashScreen(navController: NavController){
+
+    LaunchedEffect(key1 = true,){
+        delay(5000L)
+        navController.graph.startDestinationRoute?.let {
+            navController.popBackStack(
+                route = it,
+                inclusive = true
+            )
+        }
+        navController.navigate(NavigationScreen.HomePage.name)
+    }
     Column (
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
