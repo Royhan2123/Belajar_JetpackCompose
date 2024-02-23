c886d0964d319b03a3a522bb7626122a42008682
Royhan <royhankeren12@gmail.com>
2024-02-23 22:00:40 +0700

    add bottomNavigatiion

--- NewApplicationJetpackCompose/app/src/main/java/com/example/newapplicationjetpackcompose/HalamanBottom.kt
+++ NewApplicationJetpackCompose/app/src/main/java/com/example/newapplicationjetpackcompose/HalamanBottom.kt
@@ -58,7 +58,9 @@ fun HalamanBottom() {
                         selected = currentRoute == screen.title,
                         onClick = {
                             navController.navigate(screen.title) {
-
+popUpTo(navController.graph.startDestinationId){
+    saveState = true
+}
                             }
                         },
                         icon = {
