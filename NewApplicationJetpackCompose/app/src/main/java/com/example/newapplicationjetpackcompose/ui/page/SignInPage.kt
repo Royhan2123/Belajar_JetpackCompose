10cd0e672af7b076973251b28f8ebd301095f6be
Royhan <royhankeren12@gmail.com>
2024-02-26 22:41:43 +0700

    new image -google

--- NewApplicationJetpackCompose/app/src/main/java/com/example/newapplicationjetpackcompose/ui/page/SignInPage.kt
+++ NewApplicationJetpackCompose/app/src/main/java/com/example/newapplicationjetpackcompose/ui/page/SignInPage.kt
@@ -1,17 +1,22 @@
 package com.example.newapplicationjetpackcompose.ui.page
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material.Icon
+import androidx.compose.material.Surface
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VisibilityOff
@@ -30,7 +35,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
@@ -41,6 +48,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.example.newapplicationjetpackcompose.R
 import com.example.newapplicationjetpackcompose.navigation.NavigationScreen
 import com.example.newapplicationjetpackcompose.ui.theme.LightBlue
 
@@ -226,6 +234,22 @@ fun SignInPage(navController: NavController) {
                 Alignment.CenterHorizontally
             )
         )
+        Spacer(modifier = Modifier.height(20.dp))
+        Row (
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(100.dp),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(
+                id = R.drawable.google,
+            ),
+                contentDescription = "image-google",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.size(50.dp))
+        }
     }
 }
 
