82c8087b07c6ffec9d53ab44bb1861af86685253
Royhan <royhankeren12@gmail.com>
2024-02-27 23:19:34 +0700

    add latihan lazy column

--- NewApplicationJetpackCompose/app/src/main/java/com/example/newapplicationjetpackcompose/ui/page/Homepages/HomePage.kt
+++ NewApplicationJetpackCompose/app/src/main/java/com/example/newapplicationjetpackcompose/ui/page/Homepages/HomePage.kt
@@ -32,6 +32,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -94,7 +95,8 @@ fun HomePage(navController: NavController) {
                 style = TextStyle(
                     color = Color.White,
                     fontSize = 15.sp
-                )
+                ),
+                textAlign = TextAlign.Center
             )
         }
     }
