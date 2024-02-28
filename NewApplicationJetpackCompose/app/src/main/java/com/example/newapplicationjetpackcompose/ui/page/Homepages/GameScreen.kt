8ac7b42cf2ba8c96e854d1a6efff5f9914ccc4af
Royhan <royhankeren12@gmail.com>
2024-02-28 18:22:44 +0700

    game screen -update

--- NewApplicationJetpackCompose/app/src/main/java/com/example/newapplicationjetpackcompose/ui/page/Homepages/GameScreen.kt
+++ NewApplicationJetpackCompose/app/src/main/java/com/example/newapplicationjetpackcompose/ui/page/Homepages/GameScreen.kt
@@ -1,5 +1,6 @@
 package com.example.newapplicationjetpackcompose.ui.page.Homepages
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
@@ -9,12 +10,21 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ColorScheme
+import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.material3.MaterialTheme.shapes
+import androidx.compose.material3.MaterialTheme.typography
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.newapplicationjetpackcompose.R
 
 @Composable
@@ -38,14 +48,41 @@ fun GameScreen() {
 }
 
 @Composable
-fun GameLayout(){
+fun GameLayout() {
     val mediumPadding = dimensionResource(id = R.dimen.padding_medium)
 
+    Card(
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 5.dp
+        )
+    ) {
+        Column(
+            modifier = Modifier.padding(mediumPadding),
+            verticalArrangement = Arrangement.spacedBy(mediumPadding),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                modifier = Modifier
+                    .clip(shapes.medium)
+                    .background(colorScheme.surfaceTint)
+                    .padding(horizontal = 10.dp, vertical = 4.dp)
+                    .align(alignment = Alignment.End),
+                text = stringResource(id = R.string.word_count, 0),
+                style = typography.titleMedium,
+                color = colorScheme.onPrimary
+            )
+            Text(
+                text = "Scrambleum",
+                style = typography.displayMedium
+            )
+
+        }
+    }
 }
 
 @Preview
 @Composable
-fun PreviewGameLayout(){
+fun PreviewGameLayout() {
     GameLayout()
 }
 
