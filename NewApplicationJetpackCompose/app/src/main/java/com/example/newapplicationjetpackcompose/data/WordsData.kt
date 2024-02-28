9db014dc7a8c0e9bda28e02a0f52b1a45453905b
Royhan <royhankeren12@gmail.com>
2024-02-28 17:45:59 +0700

    WordsData

---
+++ NewApplicationJetpackCompose/app/src/main/java/com/example/newapplicationjetpackcompose/data/WordsData.kt
@@ -0,0 +1,22 @@
+package com.example.newapplicationjetpackcompose.data
+
+const val MAX_NO_OF_WORDS = 10
+const val SCORE_WINCREASE = 10
+
+val allWords: Set<String> =
+    setOf(
+        "animal",
+        "auto",
+        "anecdote",
+        "alphabet",
+        "all",
+        "awesome",
+        "arise",
+        "balloon",
+        "basket",
+        "bench",
+        // ...
+        "zoology",
+        "zone",
+        "zeal"
+    )
--- NewApplicationJetpackCompose/app/src/main/java/com/example/newapplicationjetpackcompose/ui/page/Homepages/HomePage.kt
+++ NewApplicationJetpackCompose/app/src/main/java/com/example/newapplicationjetpackcompose/ui/page/Homepages/HomePage.kt
@@ -2,35 +2,20 @@ package com.example.newapplicationjetpackcompose.ui.page.Homepages
 
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ElevatedButton
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -38,13 +23,9 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-import com.example.newapplicationjetpackcompose.R
-import com.example.newapplicationjetpackcompose.data.DataSource
-import com.example.newapplicationjetpackcompose.model.Affirmation
 import com.example.newapplicationjetpackcompose.navigation.NavigationScreen
 import com.example.newapplicationjetpackcompose.ui.theme.LightBlue
 
-@OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun HomePage(navController: NavController) {
