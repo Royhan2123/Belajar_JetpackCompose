4a49ed0a6e4c01ac7ca2fa060c7e9797fea43bba
Royhan <royhankeren12@gmail.com>
2024-02-29 20:36:44 +0700

    add viewModel compose dependecies

--- NewApplicationJetpackCompose/app/build.gradle.kts
+++ NewApplicationJetpackCompose/app/build.gradle.kts
@@ -69,4 +69,5 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     debugImplementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.compose.material:material:1.6.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
 }
