7ff8330985327f14b3a23e6c137404e30d072438
Royhan <royhankeren12@gmail.com>
2024-03-06 21:29:08 +0700

    add GameViewModelTest

---
+++ NewApplicationJetpackCompose/app/src/test/java/com/example/android/unscramble/ui/test/GameViewModelTest.kt
@@ -0,0 +1,7 @@
+package com.example.android.unscramble.ui.test
+
+import com.example.newapplicationjetpackcompose.viewModel.GameViewModel
+
+class GameViewModelTest  {
+    private val viewModel = GameViewModel()
+}
