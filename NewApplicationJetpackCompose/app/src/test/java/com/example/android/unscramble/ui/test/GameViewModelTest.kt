20fedec5a339b738e8865bde2ac2ad4e56880c70
Royhan <royhankeren12@gmail.com>
2024-03-06 21:30:58 +0700

    add GameViewModelTest

--- NewApplicationJetpackCompose/app/src/test/java/com/example/android/unscramble/ui/test/GameViewModelTest.kt
+++ NewApplicationJetpackCompose/app/src/test/java/com/example/android/unscramble/ui/test/GameViewModelTest.kt
@@ -1,7 +1,13 @@
 package com.example.android.unscramble.ui.test
 
 import com.example.newapplicationjetpackcompose.viewModel.GameViewModel
+import org.junit.Test
 
 class GameViewModelTest  {
     private val viewModel = GameViewModel()
+
+    @Test
+    fun gameViewModel_CorrectWordGuessed_ScoreUpdatedAndErrorFlagUnset() {
+        var currentGameUiState = viewModel.uiState.value
+    }
 }
