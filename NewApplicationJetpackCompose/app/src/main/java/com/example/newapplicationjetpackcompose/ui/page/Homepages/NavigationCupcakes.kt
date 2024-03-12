a59bbc86f48a57fb03c9e06a160dc33a7c28810a
Royhan <royhankeren12@gmail.com>
2024-03-12 18:21:37 +0700

    navigation cupcakes -updates

--- NewApplicationJetpackCompose/app/src/main/java/com/example/newapplicationjetpackcompose/ui/page/Homepages/NavigationCupcakes.kt
+++ NewApplicationJetpackCompose/app/src/main/java/com/example/newapplicationjetpackcompose/ui/page/Homepages/NavigationCupcakes.kt
@@ -3,31 +3,65 @@ package com.example.newapplicationjetpackcompose.ui.page.Homepages
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.newapplicationjetpackcompose.R
 import com.example.newapplicationjetpackcompose.data.DataSourceCupcakes
 import com.example.newapplicationjetpackcompose.viewModel.com.example.newapplicationjetpackcompose.viewModel.OrderViewModel
 
-enum class CupcakeScreen() {
-    Start,
-    Flavor,
-    Pickup,
-    Summary
+enum class CupcakeScreen(@StringRes val title: Int) {
+    Start(title = R.string.app_name),
+    Flavor(title = R.string.choose_flavor),
+    Pickup(title = R.string.choose_pickup_date),
+    Summary(title = R.string.order_summary)
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CupcakeAppBar(
+    currentScreen: CupcakeScreen,
+    canNavigateBack: Boolean,
+    navigateUp: () -> Unit = { },
+    @SuppressLint("ModifierParameter") modifier: Modifier = Modifier
+) {
+    TopAppBar(title = { Text(text = stringResource(currentScreen.title)) },
+        modifier = modifier,
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = stringResource(
+                            id = R.string.back_button
+                        )
+                    )
+                }
+            }
+        })
 }
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
@@ -37,9 +71,19 @@ fun NavigationCupcakes(
     navController: NavHostController = rememberNavController()
 ) {
 
+    val backStackEntry by navController.currentBackStackEntryAsState()
+
+    val currentScreen = CupcakeScreen.valueOf(
+        backStackEntry?.destination?.route ?: CupcakeScreen.Start.name
+    )
     Scaffold { innerPadding ->
         val uiState by viewModel.uiState.collectAsState()
 
+        CupcakeAppBar(
+            currentScreen = currentScreen,
+            canNavigateBack = navController.previousBackStackEntry != null,
+            navigateUp = { navController.navigateUp() },
+        )
         NavHost(
             navController = navController,
             startDestination = CupcakeScreen.Start.name,
@@ -79,13 +123,16 @@ fun NavigationCupcakes(
                 )
             }
             composable(route = CupcakeScreen.Summary.name) {
+                val context = LocalContext.current
                 OrderSummaryScreen(
                     orderUiState = uiState,
                     onCancelButtonClicked = {
                         cancelOrderAndNavigateToStart(viewModel, navController)
                     },
                     onSendButtonClicked = { subject: String, summary: String ->
-
+                        shareOrder(
+                            context, subject = subject, summary = summary
+                        )
                     },
                     modifier = Modifier.fillMaxHeight()
                 )
@@ -112,8 +159,8 @@ private fun shareOrder(
 
     val intent = Intent(Intent.ACTION_SEND).apply {
         type = "text/plain"
-        putExtra(Intent.EXTRA_SUBJECT,subject)
-        putExtra(Intent.EXTRA_TEXT,summary)
+        putExtra(Intent.EXTRA_SUBJECT, subject)
+        putExtra(Intent.EXTRA_TEXT, summary)
     }
 
     context.startActivity(
