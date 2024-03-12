1225cd4702ec321e43d5e05646756a196df54f49
Royhan <royhankeren12@gmail.com>
2024-03-12 18:08:47 +0700

    navigation cupcakes -updates

--- NewApplicationJetpackCompose/app/src/main/java/com/example/newapplicationjetpackcompose/ui/page/Homepages/NavigationCupcakes.kt
+++ NewApplicationJetpackCompose/app/src/main/java/com/example/newapplicationjetpackcompose/ui/page/Homepages/NavigationCupcakes.kt
@@ -1,6 +1,8 @@
 package com.example.newapplicationjetpackcompose.ui.page.Homepages
 
 import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -99,6 +101,26 @@ private fun cancelOrderAndNavigateToStart(
     viewModel.resetOrder()
     navController.popBackStack(
         CupcakeScreen.Start.name, inclusive = false
+    )
+}
+
+private fun shareOrder(
+    context: Context,
+    subject: String,
+    summary: String
+) {
+
+    val intent = Intent(Intent.ACTION_SEND).apply {
+        type = "text/plain"
+        putExtra(Intent.EXTRA_SUBJECT,subject)
+        putExtra(Intent.EXTRA_TEXT,summary)
+    }
+
+    context.startActivity(
+        Intent.createChooser(
+            intent,
+            context.getString(R.string.new_cupcake_order)
+        )
     )
 }
 
