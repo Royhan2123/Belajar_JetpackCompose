2cea0e66a253f26e233e18d063cc92c742c8a404
Royhan <royhankeren12@gmail.com>
2024-04-15 16:45:14 +0700

    add dataModels

--- GetApi/app/build.gradle.kts
+++ GetApi/app/build.gradle.kts
@@ -68,4 +68,6 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("com.airbnb.android:lottie-compose:4.0.0")
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.5.0")
 }
---
+++ GetApi/app/src/main/java/com/example/learngetapi/model/DataModel.kt
@@ -0,0 +1,6 @@
+package com.example.learngetapi.model
+
+data class DataModel(
+    val name:String,
+    val job:String
+)
