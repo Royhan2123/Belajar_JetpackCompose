cc41d8b5f5809d9f783e8399972d43b5650b970f
Royhan <royhankeren12@gmail.com>
2024-05-02 16:51:13 +0700

    add Chip in HomeScreen

--- JetpackComposeApi/app/src/main/java/com/example/jetpackcomposeapi/page/HomeScreen.kt
+++ JetpackComposeApi/app/src/main/java/com/example/jetpackcomposeapi/page/HomeScreen.kt
@@ -1,6 +1,5 @@
 package com.example.jetpackcomposeapi.page
 
-import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -16,6 +15,11 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+//noinspection UsingMaterialAndMaterial3Libraries
+import androidx.compose.material.Chip
+//noinspection UsingMaterialAndMaterial3Libraries
+import androidx.compose.material.ChipDefaults
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material3.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.CameraAlt
@@ -39,6 +43,8 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.jetpackcomposeapi.R
+import com.example.jetpackcomposeapi.ui.theme.blue
+import com.example.jetpackcomposeapi.ui.theme.grey
 import com.example.jetpackcomposeapi.ui.theme.lightGray
 import com.example.jetpackcomposeapi.widget.CardDestination
 
@@ -49,6 +55,10 @@ fun HomeScreen(
     var txfSearch by rememberSaveable {
         mutableStateOf("")
     }
+    var selectedOption by rememberSaveable {
+        mutableStateOf(listOf<String>())
+    }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -126,7 +136,102 @@ fun HomeScreen(
             },
             shape = RoundedCornerShape(size = 15.dp)
         )
-        Spacer(modifier = Modifier.height(30.dp))
+        Spacer(modifier = Modifier.height(25.dp))
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            TourismOptionChip(
+                text = "Recommended",
+                selected = selectedOption.contains("Recommended"),
+                onClick = {
+                    selectedOption = if (selectedOption.contains("Recommended")) {
+                        selectedOption.filter { it != "Recommended" }
+                    } else {
+                        selectedOption + listOf("Recommended")
+                    }
+                }
+            )
+            Spacer(modifier = Modifier.width(7.dp))
+
+            TourismOptionChip(
+                text = "Intresting",
+                selected = selectedOption.contains("Intresting"),
+                onClick = {
+                    selectedOption = if (selectedOption.contains("Intresting")) {
+                        selectedOption.filter { it != "Intresting" }
+                    } else {
+                        selectedOption + listOf("Intresting")
+                    }
+                }
+            )
+            Spacer(modifier = Modifier.width(7.dp))
+
+            TourismOptionChip(
+                text = "Nearest",
+                selected = selectedOption.contains("Nearest"),
+                onClick = {
+                    selectedOption = if (selectedOption.contains("Nearest")) {
+                        selectedOption.filter { it != "Nearest" }
+                    } else {
+                        selectedOption + listOf("Nearest")
+                    }
+                }
+            )
+        }
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            TourismOptionChip(
+                text = "Best",
+                selected = selectedOption.contains("Best"),
+                onClick = {
+                    selectedOption = if (selectedOption.contains("Best")) {
+                        selectedOption.filter { it != "Best" }
+                    } else {
+                        selectedOption + listOf("Best")
+                    }
+                }
+            )
+            Spacer(modifier = Modifier.width(7.dp))
+            TourismOptionChip(
+                text = "Cheapest",
+                selected = selectedOption.contains("Cheapest"),
+                onClick = {
+                    selectedOption = if (selectedOption.contains("Cheapest")) {
+                        selectedOption.filter { it != "Cheapest" }
+                    } else {
+                        selectedOption + listOf("Cheapest")
+                    }
+                }
+            )
+            Spacer(modifier = Modifier.width(7.dp))
+            TourismOptionChip(
+                text = "Mountains",
+                selected = selectedOption.contains("Mountains"),
+                onClick = {
+                    selectedOption = if (selectedOption.contains("Mountains")) {
+                        selectedOption.filter { it != "Mountains" }
+                    } else {
+                        selectedOption + listOf("Mountains")
+                    }
+                }
+            )
+            Spacer(modifier = Modifier.width(7.dp))
+            TourismOptionChip(
+                text = "View",
+                selected = selectedOption.contains("View"),
+                onClick = {
+                    selectedOption = if (selectedOption.contains("View")) {
+                        selectedOption.filter { it != "View" }
+                    } else {
+                        selectedOption + listOf("View")
+                    }
+                }
+            )
+        }
+        Spacer(modifier = Modifier.height(70.dp))
         Row(
             modifier = Modifier
                 .fillMaxWidth()
@@ -170,6 +275,29 @@ fun HomeScreen(
                 image = R.drawable.air_terjun_tegunungan
             )
         }
+    }
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun TourismOptionChip(
+    text: String,
+    selected: Boolean,
+    onClick: () -> Unit
+) {
+    Chip(
+        modifier = Modifier.padding(end = 8.dp),
+        colors = ChipDefaults.chipColors(
+            if (selected) blue else lightGray,
+        ),
+        onClick = onClick
+    ) {
+        Text(
+            text = text,
+            color = if (selected) Color.White else grey,
+            fontSize = 14.sp,
+            fontWeight = FontWeight.Medium
+        )
     }
 }
 
